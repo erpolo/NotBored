bff9c2d1714472ca43827f46888cd61e7ed1a340
CamSoto <camila.soto@mercadolibre.com>
2022-03-18 15:24:19 -0300

    InfoActivity, TermsAndConditionsActivity

---
+++ app/src/main/java/com/felipeycamila/notbored/TermsAndConditions.kt
@@ -0,0 +1,11 @@
+package com.felipeycamila.notbored
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TermsAndConditions : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_terms_and_conditions)
+    }
+}
